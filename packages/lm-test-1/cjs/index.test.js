c913fdeed7d1b74f3f69f250a2a997f2236395b5
Bret Comnes <bcomnes@gmail.com>
2020-12-20 11:02:08 -0700

    delete cjs folder

--- packages/lm-test-1/cjs/index.test.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const tap = (m => m.__esModule ? /* istanbul ignore next */ m.default : /* istanbul ignore next */ m)(require('tap'))
-const { foo } = require('./index.js')
-
-tap.test('exports', async (t) => {
-  t.ok(foo)
-  t.equal(foo.bar, 'baz', 'exports as expected')
-})
