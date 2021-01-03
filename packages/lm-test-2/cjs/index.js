93f09edf9f381d2f380e7eb154177dbb3c6e97ca
bcomnes <jonathan@infinitescroll.org>
2021-01-03 23:06:21 +0000

    v1.1.1

--- packages/lm-test-2/cjs/index.js
+++ packages/lm-test-2/cjs/index.js
@@ -2,5 +2,5 @@
 const foo = { bar: 'baz' }
 exports.foo = foo
 
-const someThing = { whatever: 'foo', more: 'blep' }
+const someThing = { whatever: 'foo', more: 'blep', fdsf: 'fdsf' }
 exports.someThing = someThing
