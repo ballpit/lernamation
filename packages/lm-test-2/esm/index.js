3d2f5f7fb68adb736fceccb5145624fce7031ee7
Bret Comnes <bcomnes@gmail.com>
2021-01-03 16:13:03 -0700

    try this

--- packages/lm-test-2/esm/index.js
+++ packages/lm-test-2/esm/index.js
@@ -1,3 +1,3 @@
 export const foo = { bar: 'baz' }
 
-export const someThing = { whatever: 'foo', more: 'blep', fdsf: 'fdsf' }
+export const someThing = { whatever: 'foo', more: 'blep', fdsf: 'fdsf', blab: 'bloop' }
