ed4f92f3ddbf0ab62490872e7b7445641e822f5f
Bret Comnes <bcomnes@gmail.com>
2021-01-03 16:50:11 -0700

    fdsfas

--- packages/lm-test-2/esm/index.js
+++ packages/lm-test-2/esm/index.js
@@ -1,3 +1,3 @@
 export const foo = { bar: 'baz' }
 
-export const someThing = { whatever: 'foo', more: 'blep', fdsf: 'fdsf', blab: 'bloop', fsdfds: 'fdsf', fds: 'fdsf', fdsfdsfsd: 'dfsd' }
+export const someThing = { whatever: 'foo', more: 'blep', fdsf: 'fdsf', blab: 'bloop', fsdfds: 'fdsf', fds: 'fdsf', fdsfdsfsd: 'dfsd', fdsafsdsa: 'fdsfsd' }
