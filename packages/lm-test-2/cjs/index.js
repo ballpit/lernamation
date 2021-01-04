cd377c4d39b9338798292e3969f7367303943c17
bcomnes <jonathan@infinitescroll.org>
2021-01-04 02:53:12 +0000

    v1.2.1

--- packages/lm-test-2/cjs/index.js
+++ packages/lm-test-2/cjs/index.js
@@ -4,3 +4,6 @@ exports.foo = foo
 
 const someThing = { whatever: 'foo', more: 'blep', fdsf: 'fdsf', blab: 'bloop', fsdfds: 'fdsf', fds: 'fdsf', fdsfdsfsd: 'dfsd', fdsafsdsa: 'fdsfsd' }
 exports.someThing = someThing
+
+function noop () {}
+exports.noop = noop
