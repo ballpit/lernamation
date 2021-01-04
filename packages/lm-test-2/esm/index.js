33505d3b57b740d343bed1f232b50c21946c61b8
Bret Comnes <bcomnes@gmail.com>
2021-01-03 19:54:47 -0700

    stuff

--- packages/lm-test-2/esm/index.js
+++ packages/lm-test-2/esm/index.js
@@ -3,3 +3,5 @@ export const foo = { bar: 'baz' }
 export const someThing = { whatever: 'foo', more: 'blep', fdsf: 'fdsf', blab: 'bloop', fsdfds: 'fdsf', fds: 'fdsf', fdsfdsfsd: 'dfsd', fdsafsdsa: 'fdsfsd' }
 
 export function noop () {}
+
+export function woop () {}
